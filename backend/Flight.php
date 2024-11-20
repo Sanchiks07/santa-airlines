48f1ae7c1e2c8a424b5e084e5c5b81266d9ef9a5
Sanija <ipb23.s.mitniece@vtdt.edu.lv>
2024-11-20 21:02:48 +0200

    veicu daudzas izmaiņas failā Flight.php

--- backend/Flight.php
+++ backend/Flight.php
@@ -1,11 +1,74 @@
 <?php
 
+include "Aiport.php";
+include "Aicraft.php";
+
 class Flight {
     public function __construct(public $flightCode,
                                 public $origin,
                                 public $destination,
                                 public $departureTime,
                                 public $aircraft) {
-
     }
+
+    public function getDistance() {
+        $earthRadius = 6371; // Zemes vidējais rādiuss kilometros
+
+        // Konvertējam grādus uz radiāniem
+        $platFrom = deg2rad($this->origin->platums);
+        $garFrom = deg2rad($this->origin->garums);
+        $platTo = deg2rad($this->destination->platums);
+        $garTo = deg2rad($this->destination->garums);
+
+        // Aprēķinām atšķirību
+        $platDelta = $platTo - $platFrom;
+        $garDelta = $garTo - $garFrom;
+
+        // Haversine formula
+        $a = sin($platDelta / 2) * sin($platDelta / 2) +
+             cos($platFrom) * cos($platTo) *
+             sin($lonDelta / 2) * sin($lonDelta / 2);
+        $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+        $distance = $earthRadius * $c;
+
+        return $distance; // Return the distance
+    }
+
+    public function getDuration() {
+        $averageSpeed = $this->aircraft->averageSpeed; // Vidējais ātrums lidmašīnai
+        $distance = $this->getDistance(); // Attālums starp lidostām
+
+        // Aprēķinām lidojuma ilgumu stundās
+        $flightDurationHours = $distance / $averageSpeed; // stundās
+        $flightDurationMinutes = $flightDurationHours * 60; // minūtēs
+
+        // Pievienojam 30 minūtes sagatavošanai
+        $totalDuration = $flightDurationMinutes + 30;
+
+        return $totalDuration; // Atgriežam lidojuma ilgumu minūtēs
+    }
+
+    public function getLandingTime() {
+        // Aprēķinām lidojuma ilgumu
+        $durationInMinutes = $this->getDuration();
+
+        // Izveidojam jaunu DateTime objektu, lai aprēķinātu nosēšanās laiku
+        $landingTime = clone $this->departureTime;
+        $landingTime->modify("+{$durationInMinutes} minutes"); // Pievienojam lidojuma ilgumu
+
+        // Iegūstam laika joslu no API
+        $latitude = $this->destination->latitude;
+        $longitude = $this->destination->longitude;
+
+        $timezoneApiUrl = "https://tu.proti.lv/timezones/?latitude={$latitude}&longitude={$longitude}";
+        $timezoneData = json_decode(file_get_contents($timezoneApiUrl), true);
+
+        if (isset($timezoneData['timezone'])) {
+            $timezone = $timezoneData['timezone'];
+            $landingTime->setTimezone(new DateTimeZone($timezone)); // Iestata laika joslu
+        }
+
+        return $landingTime; // Atgriežam nosēšanās laiku
+    }
+
 }
--- backend/index.php
+++ backend/index.php
@@ -3,9 +3,9 @@ include "Aircraft.php";
 include "Airport.php";
 include "Flight.php";
 
-echo "I am not alive. 🐱‍🐉<br><br>";//windows + punkts = emoji
+echo "I am not alive. 🐱‍🐉<br><br>"; // windows + punkts = emoji
 
-$manaLidmasina = new Aircraft("Airbuss", "A220-300", 120, 850);
+$manaLidmasina = new Aircraft("Airbus", "A220-300", 120, 850);
 var_dump($manaLidmasina);
 
 echo "<br><br>";
@@ -15,6 +15,16 @@ var_dump($manaAirport);
 
 echo "<br><br>";
 
-$departureTime = new DateTime('2024-11-19 15:00:00', new DateTimeZone('Europe/Riga'));
-$mansFlight = new Flight("SA503", $origin, $manaAirport, $departureTime, $manaLidmasina);
+$destination = new Airport("JFK", 40.6413, -73.7781); // Pievieno galamērķa lidostu
+$departureTime = new DateTime('2023-10-15 10:00:00', new DateTimeZone('Europe/Riga'));
+$mansFlight = new Flight("SA503", $manaAirport, $destination, $departureTime, $manaLidmasina); // Izmanto iepriekš definētos objektus
 var_dump($mansFlight);
+
+echo "<br><br><br>";
+
+echo "Flight Code: " . $mansFlight->flightCode . "<br>";
+echo "Origin: " . $mansFlight->origin->kods . " (" . $mansFlight->origin->platums . ", " . $mansFlight->origin->garums . ")<br>";
+echo "Destination: " . $mansFlight->destination->kods . " (" . $mansFlight->destination->platums . ", " . $mansFlight->destination->garums . ")<br>";
+echo "Departure Time: " . $mansFlight->departureTime->format('Y-m-d H:i:s') . "<br>";
+echo "Aircraft: " . $mansFlight->aircraft->razotajs . " " . $mansFlight->aircraft->modelis . "<br>";
+?>
